713dd73237795c5551c5128ce34852cbcf40881f
Villev00 <ville.varhala@gmail.com>
2022-04-22 14:33:41 +0300

    finnkino kuvat toimii

--- js/slideshow.js
+++ js/slideshow.js
@@ -20,41 +20,37 @@ async function getNews() {
       newsImages.push(news[i].querySelector("ImageURL").innerHTML);
     }
 
-    return newsImages;
+    await kuva(newsImages);
   } catch(error) {
     console.error(error);
   }
 }
 
-function kuva(slide) {
+async function kuva(slide) {
   let imgfirst;
   let imgsecond;
   let imgthird;
   let imgfourth;
-  console.log(slide)
-  if (slide.photos) {
-    let random = Math.floor(Math.random()*slide.photos.length);
+  console.log(slide);
+  if (slide) {
     const slide_elem = document.getElementsByClassName("slide");
     imgfirst = document.createElement('img');
-    imgfirst.src = slide.photos[random].img_src;
+    imgfirst.src = slide[0];
       
     slide_elem[0].appendChild(imgfirst);
 
-    random = Math.floor(Math.random()*slide.photos.length);
     imgsecond = document.createElement('img');
-    imgsecond.src = slide.photos[random].img_src;
+    imgsecond.src = slide[1];
      
     slide_elem[1].appendChild(imgsecond);
 
-    random = Math.floor(Math.random()*slide.photos.length);
     imgthird = document.createElement('img');
-    imgthird.src = slide.photos[random].img_src;
+    imgthird.src = slide[2];
      
     slide_elem[2].appendChild(imgthird);
 
-    random = Math.floor(Math.random()*slide.photos.length);
     imgfourth = document.createElement('img');
-    imgfourth.src = slide.photos[random].img_src;
+    imgfourth.src = slide[3];
       
     slide_elem[3].appendChild(imgfourth);
 
@@ -66,14 +62,8 @@ function kuva(slide) {
     slide_elem[0].appendChild(imgfirst);
   }
 }
-fetch('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=1RZqOOQSWmVECbPyRb3x7NRkO6JiEKqfbkSf5wGg')            
-.then(function(vastaus){       
-  return vastaus.json();       
-}).then(function(slide){       
-  kuva(slide);          
-}).catch(function(error){      
-  console.log(error);        
-}) 
+      
+getNews();
 
 let counter = 1;
 setInterval(function(){
